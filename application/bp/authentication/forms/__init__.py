e04e42e09341e785b6b788b6300d15345294ca4e
Jaynil Patel <pateljaynil20@gmail.com>
2025-04-29 21:53:05 +0500

    Initial upload of Flask Login Project

--- application/bp/authentication/forms/__init__.py
+++ application/bp/authentication/forms/__init__.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Regexp
+
+# Basic regex pattern for emails (not exhaustive but works for common cases)
+basic_email_regex = r'^[^@]+@[^@]+\.[^@]+$'
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Regexp(basic_email_regex, message="Invalid email address")
+    ])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
     pass
-
-
